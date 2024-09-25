ebb88cbb65ca422a951055f25af9851d9773eece
Ezequielmx <ezequielmx@gmail.com>
2024-09-24 21:03:31 -0300

    dash certificados looker

--- resources/views/admin/dashcert/index.blade.php
+++ resources/views/admin/dashcert/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Dash Certificados')
 
 @section('content')
-    <iframe id="frm" src="https://app.powerbi.com/view?r=eyJrIjoiYTJhZWNjMGYtM2E4Yy00OGJhLTkyZWQtMGYxYmNmNmY0NDMwIiwidCI6IjMxNWVjODU4LTYxZGUtNDJhNS1iNTEyLTNkOTQwNmI3MTJjMyJ9&pageName=ReportSection" frameborder="0" style="overflow:hidden; height: calc(100vh - 80px); width: 100%"></iframe>
+    <iframe id="frm" src="https://lookerstudio.google.com/embed/reporting/a5042d48-ec58-482b-8bf2-a4a54f338c6e/page/DmyCE" frameborder="0" style="overflow:hidden; height: calc(100vh - 80px); width: 100%"></iframe>
 @stop
 
 @section('css')
