0e7e37f7474e45dcf5491403b0836f5c825e1561
Ezequielmx <ezequielmx@gmail.com>
2024-12-10 21:07:23 -0300

    Dash asistencia en Looker

--- resources/views/admin/dashasist/index.blade.php
+++ resources/views/admin/dashasist/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Dash Asistencias')
 
 @section('content')
-    <iframe id="frm" src="https://app.powerbi.com/view?r=eyJrIjoiZTQwNzc4NWMtMTcwYS00NGVhLTlmZDEtZTU5ZWVlYjE3YTBlIiwidCI6IjMxNWVjODU4LTYxZGUtNDJhNS1iNTEyLTNkOTQwNmI3MTJjMyJ9&pageName=ReportSection" frameborder="0" style="overflow:hidden; height: calc(100vh - 80px); width: 100%"></iframe>
+    <iframe id="frm" src="https://lookerstudio.google.com/embed/reporting/4db18a4d-c05b-4339-9df5-8fdbd986f73d/page/R5xYE" frameborder="0" style="overflow:hidden; height: calc(100vh - 80px); width: 100%"></iframe>
 @stop
 
 @section('css')
